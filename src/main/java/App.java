930338f486c9d2f99b1b8d9fcda5b67b5119df1e
chunin <ziroq21@yandex.ru>
2024-08-13 10:40:35 +0300

    asdf

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,22 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+       Cat cat1 = (Cat) applicationContext.getBean("cat");
+       Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1.getMessage());
+
+        System.out.println(bean1.equals(bean2));
+        System.out.println(cat1.equals(cat2));
     }
 }
